426e27a8e851b24852386bc8df886f2f83004209
khoamdES <khoamd@eplatform.vn>
2016-12-12 15:10:10 +0700

    first commit

---
+++ ManualMemory/ManualMemory/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ManualMemory
+//
+//  Created by Khoa on 12/12/16.
+//  Copyright © 2016 Khoa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
