d81cc1f35fbc59d8510d466a22a1a64877ef21ff
khoamdES <khoamd@eplatform.vn>
2016-12-12 15:18:47 +0700

    Manual Memory

--- ManualMemory/ManualMemory/main.m
+++ ManualMemory/ManualMemory/main.m
@@ -8,10 +8,27 @@
 
 #import <Foundation/Foundation.h>
 
+@interface Person : NSObject {
+    NSString *_name;
+}
+-(void) setName: (NSString *) name;
+-(NSString *) getName;
+@end
+@implementation Person
+-(void) setName: (NSString *) name {
+    _name = name;
+}
+-(NSString *) getName {
+    return _name;
+}
+
+@end
+
+
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+    Person *person = [[Person alloc] init];
+    [person setName:@"Khoa"];
+    NSLog(@"%@", [person getName]);
+    [person release];
     return 0;
 }
